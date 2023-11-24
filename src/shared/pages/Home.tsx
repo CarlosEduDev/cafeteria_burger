3af2167b8f8f4c74034b3ee9d7118139b91f5090
Carlos Eduardo Evangelista <CarlosEduardoEvang@gmail.com>
2023-11-24 17:40:22 -0300

    feat: componente Home finalizado

--- src/shared/pages/Home.tsx
+++ src/shared/pages/Home.tsx
@@ -1,67 +1,68 @@
+import { Box, Typography, Grid, Card, CardContent } from "@mui/material";
 
-import { Box, Typography, Grid, Card, CardContent} from "@mui/material"
-
-import LunchDiningIcon from '@mui/icons-material/LunchDining';
-import DinnerDiningIcon from '@mui/icons-material/DinnerDining';
-import WineBarIcon from '@mui/icons-material/WineBar';
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import DinnerDiningIcon from "@mui/icons-material/DinnerDining";
+import WineBarIcon from "@mui/icons-material/WineBar";
 
 import { LayoutBasePagina } from "../layout";
 
-
 export const Home: React.FC = () => {
-
-  return(
+  return (
     <LayoutBasePagina titulo="Boas vindas!">
-      <Box margin={2} display='flex'>
-        <Typography variant="h5">Veja as opções que temos em Burger Snacks!</Typography>
+      <Box margin={2} display="flex">
+        <Typography variant="h5">
+          Peça o que desejar em Burger Snacks!
+        </Typography>
       </Box>
-     
 
       <Box marginLeft={2}>
         <Typography variant="h6">Nós temos:</Typography>
       </Box>
-      <Box width='100%' display='flex'>
-        
-            <Grid container margin={3}>
-               <Grid item container spacing={3}>
 
-                  <Grid item xs={12} sm={10} md={6} lg={4}  xl={3}>
-                     <Card>
-                        <CardContent>  
-                           <Typography variant="h2" align="center">
-                              <LunchDiningIcon  style={{fontSize:'2em'}}/>
-                           </Typography>
-                           <Typography align="center" fontSize={20}>Lanches</Typography>
-                           </CardContent>
-                </Card>
-              </Grid>
-
-                  <Grid item xs={12} sm={10} md={6} lg={4}  xl={3}>
-                     <Card>
-                        <CardContent>  
-                           <Typography variant="h2" align="center">
-                              <DinnerDiningIcon  style={{fontSize:'2em'}}/>
-                           </Typography>
-                           <Typography align="center" fontSize={20}>Almoços</Typography>
-                           </CardContent>
-                </Card>
-              </Grid>
-                  <Grid item xs={12} sm={10} md={6} lg={4}  xl={3}>
-                     <Card>
-                        <CardContent>  
-                           <Typography variant="h2" align="center">
-                              <WineBarIcon  style={{fontSize:'2em'}}/>
-                           </Typography>
-                           <Typography align="center" fontSize={20}>Bebidas</Typography>
-                           </CardContent>
-                </Card>
-              </Grid>
-                  
-            </Grid>
+      <Box width="100%" display="flex">
+         
+         <Grid container spacing={3} padding={0.1}>
+          <Grid item xs={12} sm={5.4} md={3.7} lg={3} xl={3}>
+            <Card>
+              <CardContent>
+                <Typography variant="h2" align="center">
+                  <LunchDiningIcon style={{ fontSize: "2em" }} />
+                </Typography>
+                <Typography align="center" fontSize={20}>
+                  Lanches
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
 
-        </Box>
+          <Grid item xs={12} sm={5.4} md={3.7} lg={3} xl={3}>
+            <Card>
+              <CardContent>
+                <Typography variant="h2" align="center">
+                  <DinnerDiningIcon style={{ fontSize: "2em" }} />
+                </Typography>
+                <Typography align="center" fontSize={20}>
+                  Almoços
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
 
+          <Grid item xs={12} sm={5.4} md={3.7} lg={3} xl={3}>
+            <Card>
+              <CardContent>
+                <Typography variant="h2" align="center">
+                  <WineBarIcon style={{ fontSize: "2em" }} />
+                </Typography>
+                <Typography align="center" fontSize={20}>
+                  Bebidas
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+       
+      </Box>
     </LayoutBasePagina>
-  )
-}
+  );
+};
--- src/shared/pages/Snacks.tsx
+++ src/shared/pages/Snacks.tsx
@@ -1,20 +1,16 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { LayoutBasePagina } from "../layout"
 
 
 
 export const Snacks: React.FC = () => {
 
-  const theme  = useTheme()
-
-  const smDown = useMediaQuery(theme.breakpoints.down('sm'))//MEDIA QUERY
-  const mdDown = useMediaQuery(theme.breakpoints.down('md'))//MEDIA QUERY
-
 
   return(
     <Box>
       <LayoutBasePagina titulo="Lanches">
         <Box>
+          <Typography>Lanches:</Typography>
 
         </Box>
       </LayoutBasePagina>
